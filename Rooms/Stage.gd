f1d45f34d4df9ef27e38f1c1fa02ac71e519f0a6
AtomicShr00m <atomicshroom14@gmail.com>
2025-11-09 12:34:10 +0100

    Stage Update
    Added spawn_enemy function

--- Rooms/Stage.gd
+++ Rooms/Stage.gd
@@ -6,5 +6,10 @@ onready var blueScn = preload("res://Objects/Enemies/Blue.tscn")
 func _ready():
 	pass
 
+func spawn_enemy():
+	var enemy=blueScn.instance()
+	enemy.target=$Prime
+	add_child(enemy)
+
 func spawn_wave():
 	pass
