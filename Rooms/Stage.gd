45fef14d908d34e0a9e4542bdd80f9c5fa5c92ee
AtomicShr00m <atomicshroom14@gmail.com>
2025-11-09 12:38:59 +0100

    Revert "Stage Update"
    This reverts commit f1d45f34d4df9ef27e38f1c1fa02ac71e519f0a6.

--- Rooms/Stage.gd
+++ Rooms/Stage.gd
@@ -6,10 +6,5 @@ onready var blueScn = preload("res://Objects/Enemies/Blue.tscn")
 func _ready():
 	pass
 
-func spawn_enemy():
-	var enemy=blueScn.instance()
-	enemy.target=$Prime
-	add_child(enemy)
-
 func spawn_wave():
 	pass
